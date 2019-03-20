61a0c325c6dc8aacaea7e049b0760d3eaf90118a
= <=>
2019-03-19 22:10:38 -0600

    Initial Commit. App Created from scratch, setting up everything in webpack config

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+//Dependencies
+import React from 'react';
+import { render } from 'react-dom'
+//Assets
+import './index.css';
+//Components
+import App from './App'
+
+render(<App/>,document.getElementById('app'));
